f3fb97cdcbff0f655f517fe4999e1c8cf77bb6e5
lovee <loveeeblossomshere@gmail.com>
2023-09-23 00:54:16 +0600

    Think about main page in app.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,20 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+
+        System.out.println("""
+                 _____          _       _     _      _  \s
+                |_   _| ___  __| | ___ | |   (_) ___| |_\s
+                  | |  / _ \\/ _` |/ _ \\| |__ | |(_-/|  _|
+                  |_|  \\___/\\__/_|\\___/|____||_|/__/ \\__|
+                """);
+
+        System.out.println("""
+                \n\n
+                1. Create Task
+                2. List of Tasks
+                3. ...
+                """);
+
     }
 }
